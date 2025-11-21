3a44d9f67bf489cca2ca44e642ad20f0ae0a6198
Andrew <andymuncey@users.noreply.github.com>
2025-11-21 11:23:52 +0000

    Initial commit

--- KeyboardObserver/ViewController.swift
+++ KeyboardObserver/ViewController.swift
@@ -1,19 +1,46 @@
-//
-//  ViewController.swift
-//  KeyboardObserver
-//
-//  Created by Andrew Muncey on 21/11/2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+        
+        //allows a tap on the background to dismiss the keyboard
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
     }
-
-
+    
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification){
+        //get height of keyboard and offset view by it
+        if let frame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+            view.frame.origin.y = -frame.height
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification){
+            view.frame.origin.y = 0
+    }
+    
+    
 }
 
