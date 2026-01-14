4f3651c7122419eb3abba920295523576cc60568
Andrew <andymuncey@users.noreply.github.com>
2026-01-14 08:45:10 +0000

    extract move method

--- KeyboardObserver/ViewController.swift
+++ KeyboardObserver/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
             object: nil
         )
         
+        view.backgroundColor = .gray
+        
         //allows a tap on the background to dismiss the keyboard
         let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(tap)
@@ -30,19 +32,23 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
     
+    private func move(view activeView: UITextField, by keyboardFrame: CGRect) {
+        let frameInView = activeView.convert(activeView.bounds, to: view)
+        let topOfKeyboard = view.frame.height - keyboardFrame.height
+        
+        if frameInView.maxY > topOfKeyboard {
+            view.frame.origin.y = -(frameInView.maxY - topOfKeyboard + 10)
+        }
+    }
+    
     @objc func keyboardWillShow(_ notification: Notification){
         
         guard
-                let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
-                let activeView = view.currentFirstResponder as? UITextField
+            let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+            let activeView = view.currentFirstResponder as? UITextField
             else { return }
 
-            let frameInView = activeView.convert(activeView.bounds, to: view)
-            let topOfKeyboard = view.frame.height - keyboardFrame.height
-
-            if frameInView.maxY > topOfKeyboard {
-                view.frame.origin.y = -(frameInView.maxY - topOfKeyboard + 10)
-            }
+        move(view: activeView, by: keyboardFrame)
 
     }
     
